1a77531d83c972a9cf8934f923786c20c2c991e3
Lionel-RAKOTOMALALA <rakotomalalalionel32@gmail.com>
2026-03-04 15:30:28 +0300

    fix: Update SMTP credentials to correct email and password for proper email functionality

--- smtpconfig.h
+++ smtpconfig.h
@@ -11,8 +11,8 @@ public:
     static constexpr int SMTP_PORT = 587;
     
     // Identifiants SMTP - À remplacer avec votre email et mot de passe
-    static constexpr const char* SMTP_EMAIL = "rakotomalalalionel32@gmail.com";
-    static constexpr const char* SMTP_PASSWORD = "nvbsaozubtexuuzd";
+    static constexpr const char* SMTP_EMAIL = "aritriniaina9@gmail.com";
+    static constexpr const char* SMTP_PASSWORD = "ovyh uamx opso btbb";
     
     // Nom de l'expéditeur
     static constexpr const char* SENDER_NAME = "Gestion Vente Materiel";
